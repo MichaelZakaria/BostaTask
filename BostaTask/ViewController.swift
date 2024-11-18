e7dc273aaeaac8df40509fbb9efdc6c3392101f0
Michael Zakaria <michaelzakaria39@gmail.com>
2024-11-18 16:19:57 +0200

    Initial Commit

---
+++ BostaTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BostaTask
+//
+//  Created by Marco on 2024-11-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
